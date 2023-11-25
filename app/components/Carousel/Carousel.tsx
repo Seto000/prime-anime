b15e8e39fac4235396d9322c952ef111f3032644
Seto <thebielplays@gmail.com>
2023-11-25 16:29:00 -0300

    Add maturity rating and checkmark svg

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -25,12 +25,14 @@ export const Carousel = () => {
         <CarouselItem
           heroImg="https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_ROW_Uncharted_CleanSlate/aec693f9-c199-4617-bd5a-a51c82266e4b._UR1920,1080_AGaverage_SX1920_FMwebp_.jpeg"
           titleImg="https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_ROW_Uncharted_CleanSlate/54164464-ed83-4d13-a80b-645305f522d8._AC_SX1000_FMwebp_.png"
+          mRating="https://m.media-amazon.com/images/G/01/vcc/maturity-ratings-logos/png/djctq/12._SY64_FMpng_.png"
         />
       </SwiperSlide>
       <SwiperSlide>
         <CarouselItem
           heroImg="https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_ROW_InvincibleS2_CleanSlate/1314776a-52e3-4f13-a1be-ac1ead2d881b._UR3840,1440_AGaverage_SX1440_FMwebp_.jpeg"
           titleImg="https://m.media-amazon.com/images/S/sonata-images-prod/SVOD_ROW_InvincibleS2_CleanSlate/29597cda-e562-47d7-8d07-dbe80e80e3e3._AC_SX1000_FMwebp_.png"
+          mRating="https://m.media-amazon.com/images/G/01/vcc/maturity-ratings-logos/png/adjctq/a18._SY64_FMpng_.png"
         />
       </SwiperSlide>
       <SwiperSlide>
--- app/components/Carousel/CarouselItem.tsx
+++ app/components/Carousel/CarouselItem.tsx
@@ -5,9 +5,10 @@ import { motion } from "framer-motion";
 type CarouselItemType = {
   heroImg: string;
   titleImg: string;
+  mRating: string;
 };
 
-export const CarouselItem = ({ heroImg, titleImg }: CarouselItemType) => {
+export const CarouselItem = ({ heroImg, titleImg, mRating }: CarouselItemType) => {
   return (
     <motion.div className="mx-auto aspect-video lg:aspect-[3/1] max-w-[92%] relative">
       <Image
@@ -17,6 +18,7 @@ export const CarouselItem = ({ heroImg, titleImg }: CarouselItemType) => {
         sizes="92vw"
         className="object-cover rounded-lg object-top"
         quality={100}
+        priority
       />
       <motion.section
         initial={{ opacity: 0, y: 30 }}
@@ -30,7 +32,7 @@ export const CarouselItem = ({ heroImg, titleImg }: CarouselItemType) => {
             delay: 0.2,
           },
         }}
-        className="w-full flex flex-col absolute z-10 bottom-0 pl-6 pr-3 pb-2 lg:max-w-[37%] 2xl:max-w-[34%] lg:top-[20%] 2xl:top-[30%] lg:pl-9 xl:pl-12 lg:pr-0 lg:pb-0"
+        className="w-full flex flex-col absolute z-10 bottom-0 pl-6 pr-3 pb-2 md:pr-4 md:pb-4 lg:max-w-[37%] 2xl:max-w-[34%] lg:top-[20%] 2xl:top-[30%] lg:pl-9 xl:pl-12 lg:pr-0 lg:pb-0"
       >
         <h2 className="max-w-[65%] lg:max-w-[75%] xl:max-w-full">
           <Link href="/">
@@ -41,15 +43,45 @@ export const CarouselItem = ({ heroImg, titleImg }: CarouselItemType) => {
               sizes="(min-width: 1540px) calc(31.23vw - 47px), (min-width: 1040px) 31.46vw, (min-width: 440px) calc(59.83vw - 23px), 50vw"
               alt="Change this later"
               quality={100}
+              priority
             />
           </Link>
         </h2>
         <div className="flex lg:gap-2 text-white font-bold text-sm md:text-base xl:text-lg mt-4 lg:mt-12 xl:mt-16">
-          <div className="mb-2 lg:mb-3">
-            <span className="mr-3">SVG</span>
+          <div className="mb-2 lg:mb-3 flex items-center">
+            <svg
+              className="mr-1 h-4 w-4 md:h-5 md:w-5"
+              viewBox="0 0 24 24"
+              role="img"
+              aria-hidden="true"
+            >
+              <title>Entitled</title>
+              <svg
+                width="24"
+                height="24"
+                fill="none"
+                color="#1a98ff"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.120 2.039 C 8.641 2.287,6.414 3.362,4.761 5.107 C 1.806 8.228,1.158 12.819,3.137 16.623 C 3.620 17.552,4.164 18.288,4.938 19.061 C 5.930 20.051,7.038 20.789,8.272 21.278 C 11.634 22.610,15.313 22.080,18.200 19.845 C 18.637 19.507,19.507 18.637,19.845 18.200 C 21.256 16.378,22.000 14.236,22.000 12.000 C 22.000 7.432,18.842 3.387,14.430 2.303 C 13.446 2.062,12.028 1.948,11.120 2.039 M17.092 8.191 C 17.410 8.341,17.660 8.592,17.816 8.920 C 17.926 9.151,17.940 9.221,17.940 9.541 C 17.940 9.869,17.928 9.927,17.805 10.181 C 17.679 10.443,17.480 10.651,14.545 13.588 C 11.578 16.558,11.406 16.723,11.140 16.848 C 10.888 16.967,10.824 16.980,10.500 16.980 C 10.176 16.980,10.112 16.967,9.860 16.848 C 9.604 16.726,9.466 16.600,8.193 15.328 C 6.915 14.051,6.794 13.918,6.672 13.660 C 6.554 13.408,6.540 13.344,6.540 13.020 C 6.540 12.700,6.554 12.631,6.664 12.400 C 6.821 12.070,7.070 11.821,7.400 11.664 C 7.631 11.554,7.700 11.540,8.020 11.540 C 8.343 11.540,8.408 11.554,8.654 11.670 C 8.891 11.782,9.036 11.907,9.714 12.578 L 10.500 13.356 13.020 10.843 C 15.629 8.240,15.687 8.188,16.110 8.081 C 16.380 8.013,16.817 8.061,17.092 8.191 "
+                  fill="currentColor"
+                  stroke="none"
+                  fillRule="evenodd"
+                ></path>
+              </svg>
+            </svg>
             <span>Included with Prime</span>
           </div>
-          <span className="ml-auto lg:ml-0">Rating</span>
+          <span className="ml-auto lg:ml-0">
+            <Image
+              className="h-6 w-6 md:h-7 md:w-7"
+              width={64}
+              height={64}
+              src={mRating}
+              alt="Maturity Rating"
+            />
+          </span>
         </div>
         <div className="hidden lg:flex lg:gap-4 xl:gap-6 lg:items-center">
           <Link href="/" className="rounded-full bg-white p-4">
