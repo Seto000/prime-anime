1b3abb416123206a0cde84e8bef4f6d8db88b113
Seto <thebielplays@gmail.com>
2023-12-09 18:16:34 -0300

    Add animation

--- app/components/header/ButtonWithDropdown.tsx
+++ app/components/header/ButtonWithDropdown.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 import { IoChevronDown } from "react-icons/io5";
 
 type ButtonWDropType = {
@@ -12,7 +13,11 @@ export const ButtonWithDropdown = ({ text, btnStyles }: ButtonWDropType) => {
   const currentPath = usePathname();
 
   return (
-    <button
+    <motion.button
+      variants={{
+        hover: { backgroundColor: "#191e25" },
+      }}
+      whileHover="hover"
       className={`flex items-center gap-1 px-3 h-full md:text-lg ${btnStyles} ${
         currentPath === `/${text.toLowerCase()}`
           ? "text-inherit border-b-[3px] border-white"
@@ -20,7 +25,13 @@ export const ButtonWithDropdown = ({ text, btnStyles }: ButtonWDropType) => {
       }`}
     >
       {text}
-      <IoChevronDown className="w-3 h-3 xl:w-5 xl:h-5 transform translate-y-0.5" />
-    </button>
+      <motion.div
+        variants={{
+          hover: { rotate: 180, transition: { type: "spring", duration: 0.3 } },
+        }}
+      >
+        <IoChevronDown className="w-3 h-3 xl:w-5 xl:h-5 transform translate-y-0.5" />
+      </motion.div>
+    </motion.button>
   );
 };
