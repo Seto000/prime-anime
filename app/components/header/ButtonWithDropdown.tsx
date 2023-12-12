2a855d85c7e72ba1c5663a7ce2401366df1f699e
Seto <thebielplays@gmail.com>
2023-12-11 21:03:25 -0300

    Start dropdown code for button component

--- app/components/header/ButtonWithDropdown.tsx
+++ app/components/header/ButtonWithDropdown.tsx
@@ -3,6 +3,8 @@
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 import { IoChevronDown } from "react-icons/io5";
+import Link from "next/link";
+import { useState } from "react";
 
 type ButtonWDropType = {
   text: string;
@@ -10,10 +12,11 @@ type ButtonWDropType = {
 };
 
 export const ButtonWithDropdown = ({ text, btnStyles }: ButtonWDropType) => {
+  const [isOpen, setIsOpen] = useState(false);
   const currentPath = usePathname();
 
   return (
-    <motion.button
+    <motion.div
       variants={{
         hover: {
           backgroundColor: "#191e25",
@@ -22,20 +25,59 @@ export const ButtonWithDropdown = ({ text, btnStyles }: ButtonWDropType) => {
         },
       }}
       whileHover="hover"
-      className={`flex items-center gap-1 lg:px-3 xl:px-4 h-full md:text-lg ${btnStyles} ${
-        currentPath === `/${text.toLowerCase()}`
-          ? "text-inherit border-b-[3px] border-white"
-          : "text-muted"
-      }`}
+      className={`flex relative h-full md:text-lg ${btnStyles}`}
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
     >
-      {text}
-      <motion.div
-        variants={{
-          hover: { rotate: 180, transition: { type: "spring", duration: 0.3 } },
-        }}
+      <button
+        className={`flex items-center gap-1 lg:px-3 xl:px-4 hover:text-white ${
+          currentPath === `/${text.toLowerCase()}`
+            ? "text-inherit border-b-[3px] border-white"
+            : "text-muted"
+        }`}
       >
-        <IoChevronDown className="w-3 h-3 xl:w-5 xl:h-5 transform translate-y-0.5" />
-      </motion.div>
-    </motion.button>
+        {text}
+        <motion.div
+          variants={{
+            hover: {
+              rotate: 180,
+              transition: { type: "spring", duration: 0.3 },
+            },
+          }}
+        >
+          <IoChevronDown className="w-3 h-3 xl:w-5 xl:h-5 transform translate-y-0.5" />
+        </motion.div>
+      </button>
+      <div
+        className={`flex-col bg-[#191e25] rounded-b-lg h-fit min-w-full top-full text-muted absolute shadow-custom ${
+          isOpen ? "flex" : "hidden"
+        }`}
+      >
+        <Link
+          href="/"
+          className="p-3 hover:bg-white hover:text-black whitespace-nowrap"
+        >
+          All
+        </Link>
+        <Link
+          href="/"
+          className="p-3 hover:bg-white hover:text-black whitespace-nowrap"
+        >
+          Movies
+        </Link>
+        <Link
+          href="/"
+          className="p-3 hover:bg-white hover:text-black whitespace-nowrap"
+        >
+          TV shows
+        </Link>
+        <Link
+          href="/"
+          className="p-3 hover:bg-white hover:text-black whitespace-nowrap rounded-b-lg"
+        >
+          Sports
+        </Link>
+      </div>
+    </motion.div>
   );
 };
